4dce0ed95d43446a223b5922fe247c79ac061b41
Mohsen <mohsens2@outlook.com>
2018-08-29 20:28:49 +0430

    Init project

---
+++ DennaForOffice/Denna.Office.Word/MasterView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Denna.Office.Word
+{
+    public partial class MasterView : UserControl
+    {
+        public MasterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DennaForOffice/Denna.Office.Word/ThisAddIn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+
+namespace Denna.Office.Word
+{
+    public partial class ThisAddIn
+    {
+        private MasterView _view;
+        private Microsoft.Office.Tools.CustomTaskPane _myCustomTaskPane;
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            _view = new MasterView();
+            _myCustomTaskPane = this.CustomTaskPanes.Add(_view, "Denna");
+            _myCustomTaskPane.Visible = true;
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
+}
